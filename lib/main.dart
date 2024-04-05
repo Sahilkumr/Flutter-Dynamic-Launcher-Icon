6120d3c8c4dc4787160c11b53ac090d1665a58a3
sahilparmar <sahilkumr.dev@gmail.com>
2024-04-05 11:24:55 +0530

    file name change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:dynamic_app_icon/screens/change_app_icon.dart';
-import 'package:dynamic_app_icon/screens/change_icon_package2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
